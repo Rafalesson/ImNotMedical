dbd395b578f63e5509e015131165cc0a863cb6e2
Rafalesson <rafaalesson35@gmail.com>
2026-02-24 16:54:54 -0300

    ajuste na data

--- apps/backend/src/templates/templates.service.ts
+++ apps/backend/src/templates/templates.service.ts
@@ -178,7 +178,7 @@ const allTemplates = {
             <h1 class="mainTitle">Atestado Médico</h1>
             <p class="mainText">
               Atesto, para os devidos fins, que o(a) paciente <strong>{{patientName}}</strong> portador(a) do CPF nº <strong>{{patientCPF}}</strong> necessita de <strong>{{durationInDays}} dia(s)</strong> de afastamento de suas atividades
-              a partir de <strong>18/02/2026</strong>, por motivos de saúde.
+              a partir de <strong>14/03/2026</strong>, por motivos de saúde.
             </p>
             <div class="details">
               <div class="detailItem"><span>Finalidade</span><p>{{purpose}}</p></div>
@@ -187,7 +187,7 @@ const allTemplates = {
           </main>
           <footer class="footer">
             <div>
-              <p>Emitido em 18/02/2026 às 17:18:34</p>
+              <p>Emitido em 14/03/2026 às 17:18:34</p>
               <p id="footer_p2">
                 Atendimento realizado conforme MP nº 2.200-2/2001,
                 <br />
